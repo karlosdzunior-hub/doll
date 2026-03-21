564c0bddd2571f0e122b6ccd0c894d17be0423db
karlosdzunior-hub <karlosdzunior@gmail.com>
2026-03-21 14:38:59 +0300

    Add files via upload

--- services/__init__.py
+++ services/__init__.py
@@ -5,5 +5,6 @@ Services package - игровые сервисы
 from .energy import EnergyService
 from .market import MarketService
 from .events import EventService
+from .chat import ChatService
 
-__all__ = ["EnergyService", "MarketService", "EventService"]
+__all__ = ["EnergyService", "MarketService", "EventService", "ChatService"]
