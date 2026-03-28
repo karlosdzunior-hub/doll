1718f47eac13a41e18ee40c68c726b01a2afb9d9
karlosdzunior-hub <karlosdzunior@gmail.com>
2026-03-28 20:03:32 +0300

    Update handlers/__init__.py - chat activity router

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,8 @@
+""" Handlers package - обработчики команд и событий """
 from .main import router
+from .chat_activity import router as chat_activity_router
+
+# Подключаем роутер активности к основному
+router.include_router(chat_activity_router)
 
 __all__ = ["router"]
